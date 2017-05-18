f11c01026be4265b29e659009c96c0739a862b87
FreakJoe <sandmensch933@web.de>
2017-05-18 17:58:37 +0200

    Switched lookup to delegate address

--- functionality/queries.py
+++ functionality/queries.py
@@ -1,16 +1,17 @@
 """Provides functionality for querying data from a rein node"""
 
 import requests
+import pybitcointools
 
 KNOWN_SERVERS = ['127.0.0.1:5000'] # 'rein1-sfo.reinproject.org:2016', 'rein2-ams.reinproject.org:2016'
 
 def does_user_exist(delprivkey):
-	"""Checks for a user's existence on all known servers based on his user id"""
+	"""Checks for a user's existence on all known servers based on his private key"""
 
-	# Convert to msin
-	msin = delprivkey
+	delegate_public_key = pybitcointools.privtopub(delprivkey)
+	delegate_address = pybitcointools.pubtoaddr(delegate_public_key)
 	for server in KNOWN_SERVERS:
-		response = requests.get('http://{}/users/exists/{}'.format(server, msin)).text
+		response = requests.get('http://{}/users/exists/{}'.format(server, delegate_address)).text
 		print(response)
 		if response == 'True':
 			return True
