8076994717da2713c16ce1cbc7edf4a2dce3a407
FreakJoe <sandmensch933@web.de>
2017-06-03 16:06:14 +0200

    Some error catching and user feedback

--- functionality/queries.py
+++ functionality/queries.py
@@ -6,51 +6,63 @@ import pybitcointools
 KNOWN_SERVERS = ['127.0.0.1:5000'] # 'rein1-sfo.reinproject.org:2016', 'rein2-ams.reinproject.org:2016'
 
 def does_user_exist(delprivkey):
-	"""Checks for a user's existence on all known servers based on his private key"""
+    """Checks for a user's existence on all known servers based on his private key"""
 
-	try:
-		delegate_public_key = pybitcointools.privtopub(delprivkey)
-		delegate_address = pybitcointools.pubtoaddr(delegate_public_key)
+    try:
+        delegate_public_key = pybitcointools.privtopub(delprivkey)
+        delegate_address = pybitcointools.pubtoaddr(delegate_public_key)
 
-	except AssertionError:
-		return False
-		
-	for server in KNOWN_SERVERS:
-		response = requests.get('http://{}/users/exists/{}'.format(server, delegate_address)).text
-		if response == 'True':
-			return True
+    except AssertionError:
+        return False
+        
+    for server in KNOWN_SERVERS:
+        try:
+            response = requests.get('http://{}/users/exists/{}'.format(server, delegate_address)).text
+            if response == 'True':
+                return True
 
-	return False
+        except:
+            pass
+
+    return False
 
 def get_active_jobs(delprivkey):
-	"""Gets all active jobs a user is involved in"""
+    """Gets all active jobs a user is involved in"""
 
-	active_jobs = []
-	previous_job_names = []
-	delegate_public_key = pybitcointools.privtopub(delprivkey)
-	for server in KNOWN_SERVERS:
-		response = requests.get('http://{}/users/by_pubkey/{}/jobs/active'.format(server, delegate_public_key)).json()
-		if 'message' not in response:
-			for job in response:
-				# Avoid adding two copies of the same job from different servers
-				if job['name'] not in previous_job_names:
-					active_jobs.append(job)
-					previous_job_names.append(job['name'])
+    active_jobs = []
+    previous_job_names = []
+    delegate_public_key = pybitcointools.privtopub(delprivkey)
+    for server in KNOWN_SERVERS:
+        try:
+            response = requests.get('http://{}/users/by_pubkey/{}/jobs/active'.format(server, delegate_public_key)).json()
+            if 'message' not in response:
+                for job in response:
+                    # Avoid adding two copies of the same job from different servers
+                    if job['name'] not in previous_job_names:
+                        active_jobs.append(job)
+                        previous_job_names.append(job['name'])
 
-	return active_jobs
+        except:
+                pass
+
+    return active_jobs
 
 def get_open_jobs():
-	"""Gets all open jobs"""
+    """Gets all open jobs"""
 
-	open_jobs = []
-	previous_job_names = []
-	for server in KNOWN_SERVERS:
-		response = requests.get('http://{}/jobs/open'.format(server)).json()
-		if 'message' not in response:
-			for job in response:
-				# Avoid adding two copies of the same job from different servers
-				if job['name'] not in previous_job_names:
-					open_jobs.append(job)
-					previous_job_names.append(job['name'])
+    open_jobs = []
+    previous_job_names = []
+    for server in KNOWN_SERVERS:
+        try:
+            response = requests.get('http://{}/jobs/open'.format(server)).json()
+            if 'message' not in response:
+                for job in response:
+                    # Avoid adding two copies of the same job from different servers
+                    if job['name'] not in previous_job_names:
+                        open_jobs.append(job)
+                        previous_job_names.append(job['name'])
 
-	return open_jobs
+        except:
+            pass
+
+    return open_jobs
--- main.py
+++ main.py
@@ -5,6 +5,7 @@ from kivy.uix.screenmanager import ScreenManager, Screen
 from functionality.Database import Database
 from functionality.Setting import Setting
 from functionality.main_screen import set_user_job_data
+from functionality.queries import KNOWN_SERVERS
 
 class MainScreen(Screen):
     pass
@@ -33,9 +34,25 @@ class ReinAndroidApp(App):
         self.sm.add_widget(MainScreen(name='MainScreen'))
         self.sm.add_widget(JobScreen(name='JobScreen'))
 
+        self.sm.current = 'SignInScreen'
         if Setting.read('delprivkey'):
             self.sm.current = 'MainScreen'
             set_user_job_data(self.sm)
+
+        server_available = False
+        for server in KNOWN_SERVERS:
+            try:
+                response = requests.get('http://{}'.format(server))
+                server_available = True
+
+            except:
+                pass
+        
+        if not server_available:
+            current_screen = self.sm.get_screen(self.sm.current)
+            print(current_screen)
+            print(current_screen.ids)
+            current_screen.ids['alertHeader'].text = 'No server is currently reachable'
 
     	return self.sm
 
